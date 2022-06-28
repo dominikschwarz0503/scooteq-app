54d317fc29d52b1f254423f6056e434b06999da4
dominikschwarz0503 <dominik.schwarz@otto.de>
2022-06-28 13:35:58 +0200

    Finished setting up the timer and cost-per-minute logic

---
+++ src/Timer.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./Timer.css";
+
+let renting = false;
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds(seconds + 1);
+
+      if (seconds === 59) {
+        setMinutes(minutes + 1);
+        setSeconds(0);
+      }
+
+      const pricePerSecond = 0.15 / 60;
+      setTotalPrice(totalPrice + pricePerSecond);
+
+      if (minutes === 59 && seconds === 59) {
+        setHours(hours + 1);
+        setMinutes(0);
+        setSeconds(0);
+      }
+    }, 100);
+
+    if (!renting) {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  });
+
+  return (
+    <div>
+      <h1 className="Timer-component">
+        Rental Duration: {hours < 10 ? "0" + hours : hours}h:
+        {minutes < 10 ? "0" + minutes : minutes}m:
+        {seconds < 10 ? "0" + seconds : seconds}s
+      </h1>
+      <p>Cost per Minute: 15ct</p>
+      <div className="Total-component">
+        <h1>Total: {totalPrice.toFixed(2)}€</h1>
+      </div>
+    </div>
+  );
+}
+export default Timer;
