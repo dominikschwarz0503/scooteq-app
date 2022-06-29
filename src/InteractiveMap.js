c3bc8ac6a866528238da4067578aecf7d486b685
dominikschwarz0503 <dominik.schwarz@otto.de>
2022-06-29 12:39:36 +0200

    Added Custom map with user location tracking and custom icons

--- src/InteractiveMap.js
+++ src/InteractiveMap.js
@@ -1,32 +1,101 @@
-import React, { Component } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet";
 import "./InteractiveMap.css";
+import ChangeMapView from "./ChangeMapView";
+import L from "leaflet";
+import greenIcon from "./scooter-icon-green.svg";
+import redIcon from "./scooter-icon-red.svg";
 
-const position = [53.4997496605889, 10.002777094191392];
+function InteractiveMap() {
+  const [userLocation, setUserLocation] = useState({
+    loaded: false,
+    coordinates: { latitude: "", longitude: "" },
+    error: "ERROR!",
+  });
 
-class InteractiveMap extends Component {
-  render() {
-    return (
-      <div className="Map-container">
-        <MapContainer
-          center={position}
-          zoom={17}
-          scrollWheelZoom={false}
-          className="Map"
+  const onSuccess = (location) => {
+    setUserLocation({
+      loaded: true,
+      coordinates: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      },
+    });
+  };
+
+  const onError = (error) => {
+    setUserLocation({
+      loaded: true,
+      error,
+    });
+  };
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      onError({
+        code: 0,
+        message: "Geolocation not supported in this browser!",
+      });
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  }, []);
+
+  //Green icon for available scooter
+  const greenScooterIcon = new L.Icon({
+    iconUrl: greenIcon,
+    iconRetinaUrl: greenIcon,
+    popupAnchor: [-0, -0],
+    iconSize: [32, 45],
+  });
+
+  //Red icon for occupied scooter
+  const redScooterIcon = new L.Icon({
+    iconUrl: redIcon,
+    iconRetinaUrl: redIcon,
+    popupAnchor: [-0, -0],
+    iconSize: [32, 45],
+  });
+
+  const position = [51.490400215903314, 0.01041623824307836];
+
+  return (
+    <div className="Map-container">
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="Map"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={[54.133708600794535, 12.059956823691344]}
+          icon={greenScooterIcon}
         >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          <Popup>
+            This scooter is available! <a href="#">Rent it</a>
+          </Popup>
+        </Marker>
+        <Marker
+          position={[54.139169451041006, 12.053405492377959]}
+          icon={redScooterIcon}
+        >
+          <Popup>This scooter is occupied</Popup>
+        </Marker>
+        {userLocation.loaded && !userLocation.error && (
+          <ChangeMapView
+            coords={[
+              userLocation.coordinates.latitude,
+              userLocation.coordinates.longitude,
+            ]}
           />
-          <Marker position={[53.49980770255701, 10.002337639029264]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
-    );
-  }
+        )}
+      </MapContainer>
+    </div>
+  );
 }
 
 export default InteractiveMap;
